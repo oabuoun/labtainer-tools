36b1d0a0c3de611fef80ec1524aa99850dcfa644
Osma Abu Oun <o.abuoun@gmal.com>
2021-08-13 20:28:30 +0100

    add build-docs command

---
+++ build_docs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# sudo apt update
+# sudo apt-get install -y pdflatex texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra
+
+# or Ubuntu 18.04
+sudo apt install texlive-full -y
