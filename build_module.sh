5531357f658ff998cb5a75c6158092f3bdcc1142
Osma Abu Oun <o.abuoun@gmal.com>
2021-08-14 13:04:38 +0100

    build the module locally

--- build_module.sh
+++ build_module.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
 #   Build
-echo "=== `date` : Start Building Labtainer Lab $1 $==="
 
+set -e
+
+echo "=== `date` : Removing old config files and docker images of  $1 ==="
+# Delete old config
+cd ~/labtainer/trunk/labs/${1}
+find . -name .gz -exec rm -rf {} \;
+
+images=$(docker images $1* -q)
+echo "--------------------------------"
+echo $images
+echo "--------------------------------"
+docker rmi $images || true
+
+#docker rmi $(docker images -f “dangling=true” -q)
+
+echo "=== `date` : Start Building Labtainer Lab $1 $==="
 cd ~/labtainer/labtainer-student
 rebuild -L -f -b ${1}
-
-#   Build Docs
-#cd ~/labtainer/trunk/labs/${1}
-#make
